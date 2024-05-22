2c023ff5a4b76feb27fc80db973dfea2295d075b
tle <tlemakuz@gmail.com>
2024-05-22 16:32:18 +0700

    feat: skillcheckpoint

--- question-2.js
+++ question-2.js
@@ -3,4 +3,24 @@ const inventory = [
   { name: "Apple", price: 35, quantity: 100 },
   { name: "Banana", price: 10, quantity: 50 },
 ];
-// เริ่มเขียนโค้ดตรงนี้
+
+inventory[0].quantity = 200;
+inventory.push({
+  name: "Orange",
+  price: 20,
+  quantity: 300
+});
+function invenPrice (totalPrice, inven) {
+  return totalPrice + inven.price;
+}
+function invenQuantity (totalQuantity, inven) {
+  return totalQuantity + inven.quantity;
+}
+totalQuantity = inventory.reduce(invenQuantity, 0);
+totalPrice = inventory.reduce(invenPrice, 0);
+
+let totalInvenPrice = (totalPrice * totalQuantity);
+
+console.log("จำนวนสินค้าทั้งหมดในสต๊อก : " + totalQuantity + " ชิ้น");
+console.log("มูลค่ารวมของสินค้าทั้งหมดในสต๊อก : " + totalInvenPrice + " บาท");
+
--- question-3.js
+++ question-3.js
@@ -1,3 +1,18 @@
-// Question #3
-let userPassword = "";
-// เริ่มเขียนโค้ดตรงนี้
+// Question #3";
+
+
+//let length = userPassword.length;
+function checkPasswordStrength (userPassword) {
+    for ( let i = 0; i <= userPassword.length; i++)
+        if (userPassword.length >= 10) {return  "strong";} 
+    else if (userPassword.length < 6) {return "weak";}
+        else {return "Medium";}
+};
+        
+//let userPassword = "swnalWadqQ";
+//console.log(checkPasswordStrength(userPassword)); // "Strong"
+let userPassword = "TechUp"
+console.log(checkPasswordStrength(userPassword)); // "Medium"
+//let userPassword = "abcde"
+//console.log(checkPasswordStrength(userPassword)); // "Weak"
+//console.log(length)
